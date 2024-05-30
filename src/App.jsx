cbafe2d69ecc30d969f120e5d36e5d0b5ee4e2c1
Rayhan Kabir <rayhan.kabir29@gmail.com>
2024-05-30 12:10:17 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,45 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState({});
   const [comments, setComments] = useState({});
+  const fetchData = async () => {
+    const postsResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const usersResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const commentsResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+
+    const usersMap = usersResponse.data.reduce((acc, user) => {
+      acc[user.id] = user;
+      return acc;
+    }, {});
+
+    const commentsMap = commentsResponse.data.reduce((acc, comment) => {
+      if (!acc[comment.postId]) {
+        acc[comment.postId] = [];
+      }
+      acc[comment.postId].push(comment);
+      return acc;
+    }, {});
+
+    setPosts(postsResponse.data.sort((a, b) => b.id - a.id));
+    setUsers(usersMap);
+    setComments(commentsMap);
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      const postsResponse = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const usersResponse = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const commentsResponse = await axios.get(
-        "https://jsonplaceholder.typicode.com/comments"
-      );
-
-      const usersMap = usersResponse.data.reduce((acc, user) => {
-        acc[user.id] = user;
-        return acc;
-      }, {});
-
-      const commentsMap = commentsResponse.data.reduce((acc, comment) => {
-        if (!acc[comment.postId]) {
-          acc[comment.postId] = [];
-        }
-        acc[comment.postId].push(comment);
-        return acc;
-      }, {});
-
-      setPosts(postsResponse.data.sort((a, b) => b.id - a.id));
-      setUsers(usersMap);
-      setComments(commentsMap);
-    };
-
     fetchData();
   }, []);
   return (
-    <>
-      <div className="content-container">
+    <div className="bg-gradient-to-b from-cyan-100 to-blue-100">
+      <h1 className="text-center py-8 font-bold">Timeline Forum</h1>
+      <hr />
+      <div className="content-container mt-4">
         <PostList posts={posts} users={users} comments={comments} />
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,17 +7,23 @@ const Post = ({ post, user, comments }) => {
 
   return (
     <div>
-      <h2 className="capitalize font-bold text-xl text-red-400">{post.title}</h2>
-      <h3>by {user?.name}</h3>
-      <p>{post.body}</p>
-      <Button onClick={() => setShowComments(!showComments)} className="bg-blue-500 text-white">
+      <h2 className="capitalize font-bold text-xl text-black">{post.title}</h2>
+      <h3 className="text-red-500">
+        <span className="italic text-gray-500 font-semibold">By</span> {user?.name}
+      </h3>
+      <p className="text-gray-700 capitalize py-2">{post.body}</p>
+      <Button
+        onClick={() => setShowComments(!showComments)}
+        className="bg-blue-500 text-white"
+      >
         {showComments ? "Hide Comments" : "Show Comments"}
       </Button>
       {showComments && (
-        <ul>
+        <ul className="px-10 py-2.5">
           {comments?.map((comment) => (
-            <li key={comment.id}>
-              <strong>{comment.name}:</strong> {comment.body}
+            <li key={comment.id} className="list-disc">
+              <strong className="capitalize">{comment.name}:</strong>
+             <span className="capitalize "> {comment.body}</span>
             </li>
           ))}
         </ul>
